b495c407960f9e8340450bf0da57f60d95d2231d
vadimtemov <128626319+vadimtemov@users.noreply.github.com>
2023-12-11 19:52:22 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+const firstletter = document.getElementById("firstletter");
+
+document.addEventListener("keydown", function(event) {
+    jump();
+})
+
+function jump () {
+    if (dino.classList != "jump") {
+        dino.classList.add("jump")
+    }
+    setTimeout(function() {
+        dino.classList.remove("jump")
+    }, 500)
+}
+
+// Игра окончена
+
+let isAlive = setInterval ( function() {
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+    if (cactusLeft < 40 && cactusLeft > 0 && dinoTop >= 140) {
+        alert("GAME OVER!!!");
+    }
+}, 10)
+
+// Удаление
+
+
+
